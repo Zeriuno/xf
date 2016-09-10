f402b0d2daffa45350de827b25fb1d85e082c895
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-09-10 21:53:55 +0200

    Code commented

--- 1-Hello-World.f
+++ 1-Hello-World.f
@@ -1,3 +1,6 @@
-program hello
+! commentary until the end of the line
+program hello ! optional declaration
+
 print *,"Hello World!"
-end program hello
+
+end program hello ! end of the principal program
