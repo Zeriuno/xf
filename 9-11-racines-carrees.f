a29ef021f2d74be4d27f3de4b1f038a66fb45d78
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-09-24 18:33:22 +0200

    Commentaires

--- 9-11-racines-carrees.f
+++ 9-11-racines-carrees.f
@@ -1,11 +1,16 @@
+! ceci est un commentaire jusqu'à la fin de la ligne
 program racines_carrees ! Du livre de Claude Delannoy, Programmer en Fortran, Eyrolles, p.2
 
-  integer :: i, nrac = 5
+ ! déclarations
+
+  integer :: i, nrac = 5 ! Le type, et puis deux variables, une avec affectation
   real ::    valeur, racine
 
-  print *, 'Bonjour - je vais vous calculer ', nrac, ' racines carrees'
-  do i = 1, nrac
-    print *, 'Donnez un nombre :'
+! instructions
+
+  print *, 'Bonjour - je vais vous calculer ', nrac, ' racines carrees'  ! Sortie * indique un format. Les ' peuveunt aussi être des "
+  do i = 1, nrac  ! Boucle: de 1 qui vaut 1 jusqu'à sa valeur nrac
+    print *, 'Donnez un nombre :' 
     read *, valeur
     if (valeur >= 0) then
         racine = sqrt (valeur)
