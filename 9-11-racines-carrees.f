28af2bf124277ab19b30cc7b92b0e5940ba3f9cc
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-09-25 18:11:19 +0200

    Commentaires

--- 9-11-racines-carrees.f
+++ 9-11-racines-carrees.f
@@ -17,13 +17,16 @@ program racines_carrees ! Du livre de Claude Delannoy, Programmer en Fortran, Ey
   do i = 1, nrac  ! Boucle: de 1 qui vaut 1 jusqu'à sa valeur nrac
     print *, 'Donnez un nombre :' 
     read *, valeur
-    if (valeur >= 0) then
-        racine = sqrt (valeur)
-        print *, 'le nombre ', valeur, ' a pour racine : ', racine
-    else
+    ! lecture depuis l'entrée standard
+    ! lecture d'un format par défaut
+    ! Valeur stockée dans la variable valeur
+    if (valeur >= 0) then  ! if statement
+        racine = sqrt (valeur) ! appel de la procédure sqrt
+        print *, 'le nombre ', valeur, ' a pour racine : ', racine ! affichage
+    else ! else
         print *, 'le nombre ', valeur, 'ne possede pas de racine'
-    end if
-  end do
+    end if ! fin du if statement
+  end do ! fin de la boucle
   print *, 'Travail termine - Au revoir'
 
 end
